5c3c650c4b50e1a5388d61f2ed544e31a7877e2c
这种靠写程序混生活的人名为程序员 <LTaoist6@gmail.com>
2019-08-26 22:12:22 +0800

    Create uni.py

---
+++ code/uni.py
@@ -0,0 +1,14 @@
+
+import requests
+import pyquery
+
+url = "https://www.un.org/zh/sections/un-charter/preamble/index.html"
+
+req = requests.get(url)
+req.encoding = 'utf8'
+text = req.text
+qd = pyquery.PyQuery(text)
+sep = "\n" + "*-" * 10 + '*'
+lmd = lambda i, v: ("" if v.tag == "h3" else '  ') + v.text + (sep if v.tag == "h3" else "  " ) 
+lines = qd(".content h3, .content li").map(lmd)
+print("\r\n".join(lines))
